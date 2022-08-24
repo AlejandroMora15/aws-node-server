64bb872406b04f00a0dd1b9d63b1cda97d11db14
Alejandro Morales Ríos <64661257+AlejandroMora15@users.noreply.github.com>
2022-08-23 20:52:22 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('De Colombia pal mundo, Alejo y Sebas');
 });
 
 server.listen(port, () => {
-  console.log(`De Colombia pal mundo, Alejo y Sebas, W con Yandel en el puerto: ${port}`);
+  console.log(`Running on port: ${port}`);
 });
